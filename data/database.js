662e7d726e603213aced5b2f762370a94c4aeb55
moff <illholdthemoff@gmail.com>
2022-03-17 21:01:22 -0400

    completed day 70

--- data/database.js
+++ data/database.js
@@ -1,19 +1,17 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
 async function connectToDatabase() {
-  const client = await MongoClient.connect(
-    'mongodb://localhost:27017'
-  );
-  database = client.db('auth-demo');
+  const client = await MongoClient.connect("mongodb://0.0.0.0:27017"); // remember this has to be this way as opposed to whatever else he has as the baseline for whatever reason i forget
+  database = client.db("auth-demo");
 }
 
 function getDb() {
   if (!database) {
-    throw { message: 'You must connect first!' };
+    throw { message: "You must connect first!" };
   }
   return database;
 }
--- routes/demo.js
+++ routes/demo.js
@@ -1,29 +1,73 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/signup', async function (req, res) {});
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"]; // not using . notation because confirm-email has a - in it, therefore not allowing it.
+  const enteredPassword = userData.password;
 
-router.post('/login', async function (req, res) {});
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12); //hashes the password with random shit, and the number is the strength of it
 
-router.get('/admin', function (req, res) {
-  res.render('admin');
+  const user = {
+    email: enteredEmail,
+    password: hashedPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user); // signifies that we want to connect to the database, create a collection called users, which will then be actually created when we add data to it, which we are also doing ehre via the insertOne method
+
+  res.redirect("/login");
 });
 
-router.post('/logout', function (req, res) {});
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail }); // determining whether the login attempt is from an existing user or not by checking their entered email against those in the database.
+
+  if (!existingUser) {
+    console.log("Could not log in, not existing user");
+    return res.redirect("/login");
+  }
+
+  const passwordsAreEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  ); // checking if the password entered matches that in the database by throwing the same algorithm used when storing it initially at the entered one.
+
+  if (!passwordsAreEqual) {
+    console.log("Wrong password DUMPASS");
+    return res.redirect("/login");
+  }
+
+  console.log("user authenticated");
+  res.redirect("/admin");
+});
+
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {});
 
 module.exports = router;
